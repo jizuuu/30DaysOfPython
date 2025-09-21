8d54abcf1f25c5c4eebf141c65b62144f4201cec
jizuuu <giovisoria3@gmail.com>
2025-09-21 22:26:58 +0800

    Add exercises for string manipulation in strings.py

--- day_4/strings.py
+++ day_4/strings.py
@@ -3,3 +3,15 @@ print(' '.join(['Thirty', 'Days', 'Of', 'Python']))
 
 # Exercise 2 with join()
 print(' '.join(['Coding', 'For', 'All'])) 
+
+company = "Coding For All"
+print(company)
+
+print(len(company))
+
+print(company.upper())
+print(company.lower())
+
+print(company.capitalize())
+print(company.title())
+print(company.swapcase())
