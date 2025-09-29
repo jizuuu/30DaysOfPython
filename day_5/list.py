58367d54fa3fd51a602b112da5f1a962dd49b6db
jizuuu <giovisoria3@gmail.com>
2025-09-29 22:08:12 +0800

    Add initial list operations and data examples in list.py

---
+++ day_5/list.py
@@ -0,0 +1,22 @@
+#1
+empty = []
+
+#2
+mobile_games = ['clash royale', 'mobile legends', 'wild rift', 'honor of kings', 'clash of clans']
+
+#3
+print(len(mobile_games))
+
+#4
+mobile_games[0]
+mobile_games[2]
+mobile_games[-1]
+
+#5
+mixed_data_types = ['King Gio Visoria', 21, 172.72, 'single', 'my address']
+
+#6
+it_companies = ['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon']
+
+#7
+print(it_companies)
